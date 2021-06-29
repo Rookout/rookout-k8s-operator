fd613961022dbc815c244280d30134572739434b
Dudi <dudicon@gmail.com>
2021-06-29 16:51:35 +0300

    making the init container part of the real CI/CD

--- .jenkins/buildDocker.sh
+++ .jenkins/buildDocker.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
-echo "Building Operator Docker..."
+echo "Building Operator Init Container Docker..."
+cd ../; make build_init_container INNER_VERSION=${NEW_VERSION}
 
+echo "Building Operator Docker..."
 cd ../; make docker-build IMG=us.gcr.io/rookout/rookout-k8s-operator:${NEW_VERSION}
--- .jenkins/pushDocker.sh
+++ .jenkins/pushDocker.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -ex
 
-echo "Pushing Operator Docker..."
+echo "Pushing Operator Init Container Docker..."
+cd ../; make push_init_container INNER_VERSION=${NEW_VERSION}
 
+echo "Pushing Operator Docker..."
 cd ../; make docker-push IMG=us.gcr.io/rookout/rookout-k8s-operator:${NEW_VERSION}
