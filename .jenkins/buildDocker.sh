a63cd9e38d82a9152f1d44171a9ee91d1eb8e679
Alexey Gutkin <alexey@rookout.com>
2023-05-09 15:55:02 +0300

    migration-to-kanico

--- .jenkins/buildDocker.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -ex
-cd ../;
-echo "Building Operator Init Container Docker..."
-make build_init_container INNER_VERSION=${NEW_VERSION}
-
-echo "Building Operator Docker..."
-make docker-build IMG=us.gcr.io/rookout/rookout-k8s-operator:${NEW_VERSION} DOCKERFILE=Dockerfile
-make docker-build IMG=us.gcr.io/rookout/rookout-k8s-operator-ubi:${NEW_VERSION} DOCKERFILE=Dockerfile.ubi
